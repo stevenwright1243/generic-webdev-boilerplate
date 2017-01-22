2fbc474372b20243d7d887fcbee139b0c3cd19a0
stevenwright1243 <steven.wright1243@gmail.com>
2017-01-22 10:15:36 -0800

    Update eslint to babel es8

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,14 @@
 module.exports = {
     "extends": "airbnb-base",
     "plugins": [
-        "import"
-    ]
+        "import",
+        "eslint-plugin-babel",
+        "eslint-plugin-flowtype"
+    ],
+    "parser": "babel-eslint",
+    "ecmaVersion": 8,
+    "env": {
+    "browser": true,
+    "node": true
+    }
 };
